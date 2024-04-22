f2b559f85ff1eaeddf6348117b61671ad81e2f12
laksh <lakshtaneja77@gmail.com>
2024-04-22 18:33:22 +0530

    Add image loading functionality to Board class

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,9 @@
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.io.IOException;
 
 public class Board extends JPanel {
     public Board() {
@@ -9,6 +11,15 @@ public class Board extends JPanel {
         setSize(getPreferredSize());
         setBackground(Color.black);
         setFocusable(true);
+    }
+
+    private void loadBackground() {
+        // load background image
+        try {
+            ImageIO.read(Board.class.getResource("background.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
